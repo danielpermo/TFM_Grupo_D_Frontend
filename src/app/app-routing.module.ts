23222df9da1bc7c72c4a8fe581c7bc477ea234f3
Daniel Perez <danielpermo@gmail.com>
2023-05-07 21:53:08 +0200

    Routing de "home", "**", y ""

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { C404Component } from './components/c404/c404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: 'full', redirectTo: '/home' },
+  { path: "home", component: HomeComponent },
+  { path: "**", component: C404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
