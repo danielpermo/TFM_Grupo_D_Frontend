ee5d9f39d012426c46f3856a8bc07ff69bd0d1bc
Daniel Pérez Moreno <danielpermo@gmail.com>
2023-05-08 13:21:34 +0200

    creación db.usuarios

---
+++ src/app/db/usuarios.db.ts
@@ -0,0 +1,38 @@
+export const usuarios: any[] = [
+    {
+        id: 1,
+        username: 'usuario1',
+        email: 'usuario1@example.com',
+        password: 'password1',
+        rol: 'admin'
+    },
+    {
+        id: 2,
+        username: 'usuario2',
+        email: 'usuario2@example.com',
+        password: 'password2',
+        rol: 'alum'
+    },
+    {
+        id: 3,
+        username: 'usuario3',
+        email: 'usuario3@example.com',
+        password: 'password3',
+        rol: 'profe'
+    },
+    {
+        id: 4,
+        username: 'usuario4',
+        email: 'usuario4@example.com',
+        password: 'password4',
+        rol: 'alum'
+    }
+];
+
+
+
+
+
+
+
+
