31045e189f30004e24221387d511ed4fb0879622
Daniel Pérez Moreno <danielpermo@gmail.com>
2023-05-08 14:25:21 +0200

    creación profesores_asignaturas.db

---
+++ src/app/db/profesores_asignaturas.db.ts
@@ -0,0 +1,22 @@
+export const profesoresAsignaturas: any[] = [
+    {
+        id: 1,
+        id_profesor: 2,
+        id_asignatura: 1
+    },
+    {
+        id: 2,
+        id_profesor: 3,
+        id_asignatura: 2
+    },
+    {
+        id: 3,
+        id_profesor: 4,
+        id_asignatura: 3
+    },
+    {
+        id: 4,
+        id_profesor: 2,
+        id_asignatura: 3
+    }
+]
