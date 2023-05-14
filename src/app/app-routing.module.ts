8dccd4e6d7222b477e6596226985427fd1ef8e49
Daniel Pérez Moreno <danielpermo@gmail.com>
2023-05-14 17:17:43 +0200

    routing login y registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { C404Component } from './components/c404/c404.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 const routes: Routes = [
   { path: "", pathMatch: 'full', redirectTo: '/home' },
   { path: "home", component: HomeComponent },
+  { path: "login", component: LoginComponent },
+  { path: "registro", component: RegistroComponent },
   { path: "**", component: C404Component }
 ];
 
