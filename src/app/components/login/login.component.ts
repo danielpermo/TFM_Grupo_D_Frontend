5cf6025c8531d4731c66c26fffcd893d2df2e541
Daniel Pérez Moreno <danielpermo@gmail.com>
2023-05-14 17:37:41 +0200

    formulario html

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  UserForm: FormGroup
+
+  constructor(private activatedRoute: ActivatedRoute) {
+    this.UserForm = new FormGroup({
+      usuario: new FormControl("", [
+        Validators.required
+      ]),
+      contraseña: new FormControl("", [
+        Validators.required
+      ])
+    }, []);
+  }
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      console.log(params);
+    })
+
+  }
+
+
+  checkControl(pControlName: string, pError: string): boolean {
+    if (this.UserForm.get(pControlName)?.hasError(pError) && this.UserForm.get(pControlName)?.touched) {
+      return true;
+    }
+    return false;
+  }
+
+
+
+
 
 }
+
+
+
