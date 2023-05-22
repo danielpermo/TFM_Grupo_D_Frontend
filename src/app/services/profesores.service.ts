535fa35db190d41577a954b60e8b777298a388e2
Daniel Pérez Moreno <danielpermo@gmail.com>
2023-05-22 02:53:05 +0200

    Actualización en servicio y componentes metiendo el httpClient

--- src/app/services/profesores.service.ts
+++ src/app/services/profesores.service.ts
@@ -1,53 +1,57 @@
 import { Injectable } from '@angular/core';
 import { profesores } from '../db/profesores.db';
 import { Profesor } from '../interfaces/profesor';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfesoresService {
 
-private profesores: Profesor[];
+  private baseUrl: string;
 
 
 
-  constructor() { 
-    this.profesores = profesores
+  constructor(private httpClient: HttpClient) {
+    this.baseUrl = 'http://localhost:3000/api/'
   }
 
   getAll(): Promise<any> {
-    return Promise.resolve(profesores);
-  }
-  
-
-  getById(pId: number): Promise<any> {
-    return Promise.resolve(this.profesores.find(profesores => profesores.id === pId));
+    return firstValueFrom(
+      this.httpClient.get('http://peticiones.online/api/contacts')
+    );
   }
 
-  create(pProfesor: Profesor): Promise<Profesor> {
-    const newId = Number(this.profesores.length + 1);
-    const newProfesor = { ...pProfesor, id: newId };
-    this.profesores.push(newProfesor);
-    return Promise.resolve(newProfesor);
-  }
 
-  update(pProfesor: Profesor): Promise<Profesor> {
-    const index = this.profesores.findIndex(profesores => profesores.id === pProfesor.id);
-    if (index !== -1) {
-      this.profesores[index] = pProfesor;
-      return Promise.resolve(pProfesor);
-    } else {
-      return Promise.reject(`Profesor with ID ${pProfesor.id} not found.`);
-    }
-  }
+  // getById(pId: number): Promise<any> {
+  //   return Promise.resolve(this.profesores.find(profesores => profesores.id === pId));
+  // }
 
-  delete(pId: number): Promise<any> {
-    const index = this.profesores.findIndex(profesores => profesores.id === pId);
-    if (index !== -1) {
-      this.profesores.splice(index, 1);
-      return Promise.resolve();
-    } else {
-      return Promise.reject(`Profesor with ID ${pId} not found.`);
-    }
-  }
+  // create(pProfesor: Profesor): Promise<Profesor> {
+  //   const newId = Number(this.profesores.length + 1);
+  //   const newProfesor = { ...pProfesor, id: newId };
+  //   this.profesores.push(newProfesor);
+  //   return Promise.resolve(newProfesor);
+  // }
+
+  // update(pProfesor: Profesor): Promise<Profesor> {
+  //   const index = this.profesores.findIndex(profesores => profesores.id === pProfesor.id);
+  //   if (index !== -1) {
+  //     this.profesores[index] = pProfesor;
+  //     return Promise.resolve(pProfesor);
+  //   } else {
+  //     return Promise.reject(`Profesor with ID ${pProfesor.id} not found.`);
+  //   }
+  // }
+
+  // delete(pId: number): Promise<any> {
+  //   const index = this.profesores.findIndex(profesores => profesores.id === pId);
+  //   if (index !== -1) {
+  //     this.profesores.splice(index, 1);
+  //     return Promise.resolve();
+  //   } else {
+  //     return Promise.reject(`Profesor with ID ${pId} not found.`);
+  //   }
+  // }
 }
--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -1,54 +1,59 @@
 import { Injectable } from '@angular/core';
 import { usuarios } from '../db/usuarios.db';
 import { Usuario } from '../interfaces/usuario';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
 
-  private usuarios: Usuario[];
+  private baseUrl: string;
+  usuarios: any = {};
 
 
 
-  constructor() { 
-    this.usuarios = usuarios
+  constructor(private httpClient: HttpClient) {
+    this.baseUrl = 'http://localhost:3000/api/usuarios/login'
   }
 
-  getAll(): Promise<any> {
-    return Promise.resolve(usuarios);
-  }
-  
-
-  getById(pId: number): Promise<any> {
-    return Promise.resolve(this.usuarios.find(usuarios => usuarios.id === pId));
+  login(pUser: any): Promise<any> {
+    return firstValueFrom(
+      this.httpClient.post(this.baseUrl, pUser)
+    );
   }
 
-  create(pUsuario: Usuario): Promise<Usuario> {
-    const newId = Number(this.usuarios.length + 1);
-    const newUsuario = { ...pUsuario, id: newId };
-    this.usuarios.push(newUsuario);
-    return Promise.resolve(newUsuario);
-  }
 
-  update(pUsuario: Usuario): Promise<Usuario> {
-    const index = this.usuarios.findIndex(usuarios => usuarios.id === pUsuario.id);
-    if (index !== -1) {
-      this.usuarios[index] = pUsuario;
-      return Promise.resolve(pUsuario);
-    } else {
-      return Promise.reject(`User with ID ${pUsuario.id} not found.`);
-    }
-  }
+  // getById(pId: number): Promise<any> {
+  //   return Promise.resolve(this.usuarios.find(usuarios => usuarios.id === pId));
+  // }
 
-  delete(pId: number): Promise<any> {
-    const index = this.usuarios.findIndex(usuarios => usuarios.id === pId);
-    if (index !== -1) {
-      this.usuarios.splice(index, 1);
-      return Promise.resolve();
-    } else {
-      return Promise.reject(`User with ID ${pId} not found.`);
-    }
-  }
+  // create(pUsuario: Usuario): Promise<Usuario> {
+  //   const newId = Number(this.usuarios.length + 1);
+  //   const newUsuario = { ...pUsuario, id: newId };
+  //   this.usuarios.push(newUsuario);
+  //   return Promise.resolve(newUsuario);
+  // }
+
+  // update(pUsuario: Usuario): Promise<Usuario> {
+  //   const index = this.usuarios.findIndex(usuarios => usuarios.id === pUsuario.id);
+  //   if (index !== -1) {
+  //     this.usuarios[index] = pUsuario;
+  //     return Promise.resolve(pUsuario);
+  //   } else {
+  //     return Promise.reject(`User with ID ${pUsuario.id} not found.`);
+  //   }
+  // }
+
+  // delete(pId: number): Promise<any> {
+  //   const index = this.usuarios.findIndex(usuarios => usuarios.id === pId);
+  //   if (index !== -1) {
+  //     this.usuarios.splice(index, 1);
+  //     return Promise.resolve();
+  //   } else {
+  //     return Promise.reject(`User with ID ${pId} not found.`);
+  //   }
+  // }
 }
 
