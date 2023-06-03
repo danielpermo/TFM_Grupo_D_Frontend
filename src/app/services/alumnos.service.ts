0fe937dfe7d7dc559ddfba4d90800dd24400a9b4
Wale Obied <waleobied10@gmail.com>
2023-06-04 00:14:37 +0200

    feat(alumnos-service): add getById function and IUser interface

--- src/app/services/alumnos.service.ts
+++ src/app/services/alumnos.service.ts
@@ -1,24 +1,27 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom, lastValueFrom } from 'rxjs';
+import { IAlumno } from '../interfaces/alumno';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlumnosService {
 
-  private baseUrl: string;
+  private baseUrl: string = "http://localhost:3000/api/alumnos/";
 
 
 
-  constructor(private httpClient: HttpClient) {
-    this.baseUrl = 'http://localhost:3000/api/'
-  }
+  constructor(private httpClient: HttpClient) {}
 
   getAllPublic(): Promise<any> {
     return firstValueFrom(
       this.httpClient.get(`${this.baseUrl}/alumnos`)
     );
+  }
+
+  getById(pId: number): Promise<IAlumno> {
+    return lastValueFrom(this.httpClient.get<IAlumno>(`${this.baseUrl}${pId}`))
   }
 
 
