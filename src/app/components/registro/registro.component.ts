d35d78f236eb9e192e3e2da5a11cbaf2b5c1279d
Daniel Pérez Moreno <danielpermo@gmail.com>
2023-06-05 01:51:58 +0200

    Corrección en registro

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -93,6 +93,7 @@ export class RegistroComponent {
 
   async onSubmit() {
     let user: any = this.RegistroForm.value;
+    console.log(user)
     const response = await this.usuariosService.registro(user)
     console.log(response);
     if (response.fatal) {
