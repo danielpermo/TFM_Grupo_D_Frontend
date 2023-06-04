e6d53aded3a41047f5ecc41d50ae260e64a09a25
Daniel Pérez Moreno <danielpermo@gmail.com>
2023-06-05 01:57:40 +0200

    routing en el menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
   { path: "login", component: LoginComponent },
   { path: "registro", component: RegistroComponent },
   { path: "alum/:userId", component: AlumnoComponent, canActivate: [loginGuard] },
-  { path: "profe/:userId", component: ProfesorComponent,/* canActivate: [loginGuard]*/ },
+  { path: "profe/:userId", component: ProfesorComponent, canActivate: [loginGuard] },
   { path: "admin/:userId", component: AdminComponent, canActivate: [loginGuard, roleGuard] },
   { path: "admin/:userId/alumnos", component: AdminComponent, canActivate: [loginGuard, roleGuard] },
   { path: "admin/:userId/profesores", component: AdminComponent, canActivate: [loginGuard, roleGuard] },
