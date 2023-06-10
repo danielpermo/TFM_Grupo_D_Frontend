fdee3371ef69405769e310fab5038d710163eb19
Wale Obied <waleobied10@gmail.com>
2023-06-10 11:13:49 +0200

    feat(calificaciones-component): add new component

---
+++ src/app/components/calificaciones/calificaciones.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calificaciones',
+  templateUrl: './calificaciones.component.html',
+  styleUrls: ['./calificaciones.component.css']
+})
+export class CalificacionesComponent {
+
+}
