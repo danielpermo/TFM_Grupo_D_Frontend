0ae42333597cc64ab1ea5cc9814d23dc52c8e92b
Daniel Pérez Moreno <danielpermo@gmail.com>
2023-06-14 02:03:48 +0200

    Mis valoraciones a falta de meter valoraciones en el back

--- src/app/components/valoraciones-prof/valoraciones-prof.component.ts
+++ src/app/components/valoraciones-prof/valoraciones-prof.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlumnosService } from 'src/app/services/alumnos.service';
+import { ProfesoresService } from 'src/app/services/profesores.service';
 
 @Component({
   selector: 'app-valoraciones-prof',
   templateUrl: './valoraciones-prof.component.html',
   styleUrls: ['./valoraciones-prof.component.css']
 })
-export class ValoracionesProfComponent {
+export class ValoracionesProfComponent implements OnInit {
+
+  alumArr: any = [];
+
+  constructor(private profesoresService: ProfesoresService) { }
+
+  async ngOnInit() {
+    const response = await this.profesoresService.getProfeAlum();
+    this.alumArr = response;
+    console.log(this.alumArr);
+    return this.alumArr;
+  }
 
 }
