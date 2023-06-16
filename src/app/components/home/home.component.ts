a822d43e6d43312bec5168f23e34829f5328b075
Daniel Pérez Moreno <danielpermo@gmail.com>
2023-06-16 03:10:51 +0200

    Listado de asignaturas

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -15,9 +15,12 @@ export class HomeComponent implements OnInit {
   profesArrImp: Profesor[] = [];
   combinedArr: any = [];
   ciudadesArr: string[] = [];
-  filtroArr: any = [] = [];
+  asignaturasArr: any[] = [];
+  filtradoArr: any[] = [];
+  filtroAsignaturasArr: any[] = []
   filtradoCiudad: boolean = false;
   asignaturasActivate: boolean = false;
+  ciudadActivate: boolean = false;
 
   profesoresService = inject(ProfesoresService);
 
@@ -52,15 +55,25 @@ export class HomeComponent implements OnInit {
     this.ciudadesArr = Array.from(ciudadesSet).sort();
     this.ciudadesArr.unshift('Todas');
 
+    const asignaturasSet = new Set<string>();
+    for (const profesor of this.profesArr) {
+      for (const asignatura of profesor.asignaturas) {
+        asignaturasSet.add(asignatura);
+      }
+    }
+    this.asignaturasArr = Array.from(asignaturasSet).sort();
+    const todas: any = { asignatura_id: 0, nombre: "Todas", clase: 0 }
+    this.asignaturasArr.unshift(todas);
+
   }
 
-  async filtrar(pCiudad: string) {
-    this.filtroArr = [];
+  async filtrarCiudad(pCiudad: string) {
+    this.filtradoArr = [];
     if (pCiudad === 'Todas') {
       this.asignaturasActivate = false;
-      return this.filtroArr = this.combinedArr;
+      return this.filtradoArr = this.combinedArr;
     }
-    const response = await this.profesoresService.getAllPublic();
+    const response = await this.profesoresService.filtrarCiudad(pCiudad);
     response.sort((a: { puntuacion: string; }, b: { puntuacion: string; }) => {
       // Comprobar si a y b tienen la puntuación "No valorado"
       if (a.puntuacion === "No valorado" && b.puntuacion === "No valorado") {
@@ -82,12 +95,73 @@ export class HomeComponent implements OnInit {
     const profesArrImp: any[] = profesFiltradoArr.filter((_, index) => index % 2 === 1);
     const maxLength = Math.max(profesArrPar.length, profesArrImp.length);
     for (let i = 0; i < maxLength; i++) {
-      this.filtroArr.push({ par: profesArrPar[i], impar: profesArrImp[i] });
+      this.filtradoArr.push({ par: profesArrPar[i], impar: profesArrImp[i] });
       this.filtradoCiudad = true;
       this.asignaturasActivate = true;
-      return this.filtroArr;
+      return this.filtradoArr;
     }
 
+  }
+
+  async filtrarAsignatura(pAsignatura: any) {
+    this.filtroAsignaturasArr = [];
+    console.log(this.ciudadActivate)
+    console.log(pAsignatura)
+    if (pAsignatura === 0 && this.ciudadActivate === false) {
+      this.asignaturasActivate = false;
+      return this.filtradoArr = this.combinedArr;
+    }
+    if (pAsignatura === 0 && this.ciudadActivate === true) {
+      this.asignaturasActivate === false;
+      return this.filtradoArr;
+    }
+    if (pAsignatura === 0) {
+      return this.asignaturasActivate === false;
+    }
+    const response = await this.profesoresService.filtrarAsignatura(pAsignatura);
+    response.sort((a: { puntuacion: string; }, b: { puntuacion: string; }) => {
+      // Comprobar si a y b tienen la puntuación "No valorado"
+      if (a.puntuacion === "No valorado" && b.puntuacion === "No valorado") {
+        return 0;
+      }
+      if (a.puntuacion === "No valorado") {
+        return 1; // Mover 'a' al final
+      }
+      if (b.puntuacion === "No valorado") {
+        return -1; // Mover 'b' al final
+      }
+
+      // Ordenar por puntuación de mayor a menor
+      return Number(b.puntuacion) - Number(a.puntuacion);
+    });
+    const profesArr: any[] = response;
+    const profesFiltradoArr: any[] = profesArr.filter(profesor => profesor.asignaturas.some((asignatura: any) => asignatura.asignatura_id === pAsignatura));
+
+    const profesArrPar: any[] = profesFiltradoArr.filter((_, index) => index % 2 === 0);
+    const profesArrImp: any[] = profesFiltradoArr.filter((_, index) => index % 2 === 1);
+    const maxLength = Math.max(profesArrPar.length, profesArrImp.length);
+    for (let i = 0; i < maxLength; i++) {
+      this.filtradoArr.push({ par: profesArrPar[i], impar: profesArrImp[i] });
+      this.filtradoCiudad = true;
+      this.asignaturasActivate = true;
+      return this.filtradoArr;
+    }
+
+  }
+
+
+  mostrarFiltro(pFiltro: string) {
+    if (pFiltro === "todos") {
+      this.asignaturasActivate = false;
+      this.ciudadActivate = false;
+      this.filtradoArr = this.combinedArr;
+    } else if (pFiltro === 'ciudad') {
+      this.ciudadActivate = true;
+      this.asignaturasActivate = false;
+    } else if (pFiltro === 'asignaturas') {
+      this.asignaturasActivate = true;
+      this.ciudadActivate = false;
+    }
   }
 
 }
