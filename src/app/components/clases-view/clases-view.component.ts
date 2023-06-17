784f84c86d79c0383714329ee5244a3af5aa7532
Wale Obied <waleobied10@gmail.com>
2023-06-17 17:43:23 +0200

    feat(clases-view-component): show clases table correctly

--- src/app/components/clases-view/clases-view.component.ts
+++ src/app/components/clases-view/clases-view.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import jwtDecode from 'jwt-decode';
-import { AdministradoresService } from 'src/app/services/administradores.service';
 import { AlumnosService } from 'src/app/services/alumnos.service';
 import { AsignaturasService } from 'src/app/services/asignaturas.service';
 import { ClasesService } from 'src/app/services/clases.service';
@@ -22,17 +21,16 @@ export class ClasesViewComponent {
   finalizada: string = "";
   myUser: any = {};
   profile: string = "";
-
-
-
+  InformacionClases: any[] = [[]];
+  clases: any[] = [];
+  rowObj: any = {};
 
   constructor( private router: Router,
     private asignaturasService: AsignaturasService, 
     private clasesService: ClasesService, 
     private usuariosService: UsuariosService,
     private profesoresService: ProfesoresService,
-    private alumnosService: AlumnosService,
-    private administradoresService: AdministradoresService){}
+    private alumnosService: AlumnosService){}
 
   async ngOnInit() {
     this.getAsignaturas();
@@ -54,10 +52,6 @@ export class ClasesViewComponent {
       this.myUser = this.myUser[0];
       return this.myUser;
     }
-    const response = await this.administradoresService.getById(tokenDecode.usuario_id);
-    this.profile = "ADMINISTRADOR";
-    this.myUser = response;
-    return this.myUser;
   }
 
   async getAsignaturas() {
@@ -68,56 +62,64 @@ export class ClasesViewComponent {
       console.error(error);
     }
   }
-  
 
   async getClases() {
     try {
       const data: any[] = await this.clasesService.getAll();
       const usuarioLogadoId: number = this.usuariosService.getId();
-      const usuarios: any = this.usuariosService.getAll();
-      console.log(usuarios);
-  
+      const asig: any[] = await this.asignaturasService.getAll();
       const profesores: any = await this.profesoresService.getAllPublic();
-      console.log(profesores);
-  
-      if (this.myUser.rol === 'alum') {
-        this.clasesArr = data
-          .map((clase: any) => {
-            const asignatura = this.asignaturasArr.find(
-              (asignatura: any) => asignatura.id === clase.asignatura_id
-            );
-  
-            return {
-              ...clase,
-              asignatura_nombre: asignatura ? asignatura.nombre : '',
-              asignatura_rama: asignatura ? asignatura.rama : '',
-              estado_asignatura: clase.finalizado ? 'Finalizado' : 'En curso',
-             // profesor: asignatura.profesor_id === usuarios.id ? usuarios.nombre : ''
-            };
+      if (this.myUser.rol === 'alum') {   
+        data.forEach((clase) => {
+          if (clase.alumno_id===usuarioLogadoId){
+            this.InformacionClases[0].push(clase);
+          }
+        })
+        this.InformacionClases[1]=[];
+        this.clases=this.InformacionClases[0];
+        profesores.forEach((element: any)=>{
+          this.clases.forEach((element2: any)=>{
+            if (element.id===element2.profesor_id){              
+              this.InformacionClases[1].push((element.nombre+' '+  element.apellidos)); 
+            }         
           })
-          .filter((clase: any) => clase.alumno_id === usuarioLogadoId);
+        })
+        this.InformacionClases[2]=[];
+        this.InformacionClases[0].forEach((asignatura: any)=>{
+          asig.forEach((asigna)=>{
+            if(asignatura.asignatura_id===asigna.id){
+              this.InformacionClases[2].push(asigna.nombre);
+            }
+          })
+        })
+        let i=0;
+        while ( i < this.InformacionClases.length){
+          this.clasesArr[i]=Object.assign({}, [this.InformacionClases[2][i],this.InformacionClases[1][i],this.InformacionClases[0][i].finalizado ? 'Finalizado' : 'En curso']); 
+          i++;
+        }
       } else if (this.myUser.rol === 'profe') {
-        this.clasesArr = data
-          .map((clase: any) => {
-            const asignatura = this.asignaturasArr.find(
-              (asignatura: any) => asignatura.id === clase.asignatura_id
-            );
-  
-            return {
-              ...clase,
-              asignatura_nombre: asignatura ? asignatura.nombre : '',
-              asignatura_rama: asignatura ? asignatura.rama : '',
-              estado_asignatura: clase.finalizado ? 'Finalizado' : 'En curso',
-              //profesor: clase.profesor_id === usuarioLogadoId ? usuarios.nombre : ''
-            };
+        data.forEach((clase) => {
+          if (clase.profesor_id===usuarioLogadoId){
+            this.InformacionClases[0].push(clase);
+          }
+        })
+        this.InformacionClases[1]=[];
+        this.InformacionClases[0].forEach((asignatura: any )=>{
+          asig.forEach((asigna)=>{
+            if(asignatura.asignatura_id===asigna.id){
+              this.InformacionClases[1].push(asigna.nombre);
+            }
           })
-          .filter((clase: any) => clase.profesor_id === usuarioLogadoId);
+        })
+        let i=0;
+        while ( i <= this.InformacionClases.length){
+          this.clasesArr[i]=Object.assign({}, [this.InformacionClases[1][i],this.InformacionClases[0][i].finalizado ? 'Finalizado' : 'En curso']); 
+          i++;
+        }
       }
-    } catch (error) {
-      console.error(error);
-    }
+      }
+      catch (error) {
+       console.error(error);
+     }
   }
-  
-
-
 }
